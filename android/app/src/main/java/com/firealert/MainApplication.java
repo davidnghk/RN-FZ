81c7e8a467624bae2822ab679c604fc51b973135
HeiLeeFA98 <113403989+HeiLeeFA98@users.noreply.github.com>
2022-11-07 13:13:34 +0800

    Huawei 20221107

--- android/app/src/main/java/com/firealert/MainApplication.java
+++ android/app/src/main/java/com/firealert/MainApplication.java
@@ -15,6 +15,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.airbnb.android.react.maps.MapsPackage;  
+import com.huawei.hms.rn.push.HmsPushPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // packages.add(new HmsPushPackage());
           new MapsPackage()  ;
           return packages;
         }
